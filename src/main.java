8f00fc58d0260d46cfecf02944ee52b84580a309
languard <languard@gmail.com>
2026-01-16 11:38:47 -0500

    Assignment

--- src/main.java
+++ src/main.java
@@ -3,7 +3,7 @@ public class main {
 
 	public static void main(String[] args) {
 		// Remove comment so the line prints to the console
-		//System.out.println("Hello, world. This time, from Eclipse!");
+		System.out.println("Hello, world. This time, from Eclipse!");
 	}
 
 }
