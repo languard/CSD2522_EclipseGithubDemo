6a68d47b8821f99cf1176d82383dd8fb1782311e
languard <languard@gmail.com>
2026-01-16 09:42:24 -0500

    Uncomment print statement in main.java
    Uncommented the print statement to display the message.

--- src/main.java
+++ src/main.java
@@ -2,8 +2,8 @@
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello, world. This time, from Eclipse!");
+		//Remove the comment so it prints the message to the console
+		//System.out.println("Hello, world. This time, from Eclipse!");
 	}
 
 }
